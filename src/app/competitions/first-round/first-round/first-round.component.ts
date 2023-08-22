44c2d892ba0a609b0c3afa57f51fba5588033d48
pnik1ta <nikitap0zd3y3v@gmail.com>
2023-08-22 15:45:11 +0300

    Add layout and routing

---
+++ src/app/competitions/first-round/first-round/first-round.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-first-round',
+  templateUrl: './first-round.component.html',
+  styleUrls: ['./first-round.component.scss']
+})
+export class FirstRoundComponent {
+
+}
