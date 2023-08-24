ce13287dc0dc820b525b5d31a25e31e6a59fd71d
pnik1ta <nikitap0zd3y3v@gmail.com>
2023-08-24 16:42:32 +0300

    Finish Seeding and Up/Down page

---
+++ src/app/components/table-up-down/table-up-down.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-table-up-down',
+  templateUrl: './table-up-down.component.html',
+  styleUrls: ['./table-up-down.component.scss']
+})
+export class TableUpDownComponent {
+
+}
