8da8dc5d3dfe3a8c7c78715443798e1e837e6e60
pnik1ta <nikitap0zd3y3v@gmail.com>
2023-08-24 18:05:22 +0300

    Add general page

--- src/app/competitions/competitions-routing.module.ts
+++ src/app/competitions/competitions-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 import { CompetitionsLayoutComponent } from "./competitions-layout/competitions-layout.component";
+import { GeneralComponent } from "./general/general.component";
 
 const routes: Routes = [
   {
@@ -10,6 +11,10 @@ const routes: Routes = [
       {
         path: 'FirstRound',
         loadChildren: () => import('./first-round/first-round.module').then(m => m.FirstRoundModule),
+      },
+      {
+        path: 'General',
+        component: GeneralComponent
       }
     ]
   }
--- src/app/competitions/competitions.module.ts
+++ src/app/competitions/competitions.module.ts
@@ -3,6 +3,7 @@ import { CompetitionsRoutingModule } from "./competitions-routing.module";
 import { CompetitionsLayoutComponent } from './competitions-layout/competitions-layout.component';
 import { AsideComponent } from "../components/aside/aside.component";
 import { CommonModule } from "@angular/common";
+import { GeneralComponent } from './general/general.component';
 
 @NgModule({
   imports: [
@@ -12,6 +13,7 @@ import { CommonModule } from "@angular/common";
   declarations: [
     CompetitionsLayoutComponent,
     AsideComponent,
+    GeneralComponent,
   ],
 })
 export class CompetitionsModule {}
